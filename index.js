e463be32c3d36c673e9bf86e3d133d6095e16805
adi <adielw8@gmail.com>
2021-03-24 08:49:57 +0200

    adding gitignore

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const express = require('express')
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 
